f0014a37556078b083beea2773d6818a12c13bd5
joacoros <126888355+joacoros@users.noreply.github.com>
2023-04-14 10:33:42 -0300

    Punto 2 TP0

---
+++ Punto_2_TP0/Punto_2_TP0.ino
@@ -0,0 +1,38 @@
+#include <DHT.h>
+#include <DHT_U.h>
+
+#include <Wire.h>     // libreria para bus I2C
+#include <Adafruit_GFX.h>   // libreria para pantallas graficas
+#include <Adafruit_SSD1306.h>   // libreria para controlador SSD1306
+
+#define ANCHO 128     
+#define ALTO 64       
+
+#define OLED_RESET 4      
+Adafruit_SSD1306 oled(ANCHO, ALTO, &Wire, OLED_RESET);  // crea objeto
+
+#include "DHT.h"
+#include <DHT.h>
+#include <DHT_U.h>
+
+#define DHTPIN 23     // Digital pin connected to the DHT sensor
+
+#define DHTTYPE DHT11   // DHT 22  (AM2302), AM2321
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Wire.begin();         // inicializa bus I2C
+  oled.begin(SSD1306_SWITCHCAPVCC, 0x3C); // inicializa pantalla con direccion 0x3C
+  dht.begin();
+}
+
+void loop() {
+  float temperatura = dht.readTemperature();
+  oled.clearDisplay();      // limpia pantalla
+  oled.setTextColor(WHITE);   
+  oled.setCursor(0, 20);     // ubica cursor en inicio de coordenadas 0,0
+  oled.setTextSize(3);      
+  oled.print(temperatura);  // escribe en pantalla el texto
+  oled.display();     // muestra en pantalla todo lo establecido anteriormente
+}
